38cccfcfb6c2e6db5f43e8a169bda18dda6af899
HakanKavci <hackankavci0@gmail.com>
2023-01-30 22:03:54 +0300

    iyileştirme yapıldı

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const input = document.getElementById("input")
 const button = document.getElementById("btn")
 let city;
 
+window.addEventListener("load", ()=>{
+    input.focus()
+})
+
 button.addEventListener("click", ()=> {
     if (input.value.trim() != "") {
         city = input.value
@@ -24,6 +28,7 @@ const getWeatherInfo = async () => {
     
     try {
         const response = await fetch(URL)
+        // console.log(response);
         if (!response.ok) {
             throw new Error(`Something went wrong:${response.status}`)
         }
@@ -36,26 +41,33 @@ const getWeatherInfo = async () => {
 }
 
 const cityList = document.querySelector(".city-list")
+let cities = []
 
 const renderInfo = (data) => {
     const temperature = Math.round(data.main.temp - 273.15)
     console.log(temperature)
 
     let cityName = data.name
-
+    
     if (data.name.endsWith(" Province")) {
         cityName = data.name.replace(" Province", "")
     }
-
+    
+    if (cities.includes(data.name)) {
+        alert(`Daha önce ${cityName} hakkında sorgulama yapmıştınız!..`)
+        return
+    }
+    cities.push(data.name)
+    
     cityList.innerHTML += `
-        <li class="city-weather">
-            <p class="city-name">
-                ${cityName} ${data.sys.country}
-            </p>
-            <h1 class="city-temprature">${temperature} °C</h1>
-            <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" class="img">
-            <p class="description">${data.weather[0].description}</p>
-        </li>
+    <li class="city-weather">
+    <p class="city-name">
+    ${cityName} <span>${data.sys.country}</span>
+    </p>
+    <h1 class="city-temprature">${temperature} <span>°C</span></h1>
+    <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" class="img">
+    <p class="description">${data.weather[0].description}</p>
+    </li>
     `
 }
 
